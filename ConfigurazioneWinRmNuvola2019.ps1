da127e164facde7f48bfef532f1fb1ebe43db9d9
nuvola-github <53942668+nuvola-github@users.noreply.github.com>
2019-08-14 10:20:48 +0200

    Configurazione di WinRm con PowerShell 3.0

---
+++ ConfigurazioneWinRmNuvola2019.ps1
@@ -0,0 +1,9 @@
+﻿$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+Enable-PSremoting –SkipNetworkProfileCheck  
+Enable-WSManCredSSP -Role Server -Force
+
+powershell.exe -ExecutionPolicy ByPass -File $file
