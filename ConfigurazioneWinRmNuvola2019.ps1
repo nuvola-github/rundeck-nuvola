5f66ab18affcdd64c7a4bc7393a69bf7ff0faf52
nuvola-github <53942668+nuvola-github@users.noreply.github.com>
2019-08-14 15:44:49 +0200

    Commentato

--- ConfigurazioneWinRmNuvola2019.ps1
+++ ConfigurazioneWinRmNuvola2019.ps1
@@ -1,14 +1,22 @@
 
 # Eseguire queste righe in Powershell ISE 3.0
 
+# Controlla che la versionedi powershell  sia almento 4.0
+$PSVersionTable
+
+# Abilita l'elesuzione dello script di cui verrà fatto il download
 set-executionpolicy remotesigned
 
+# Esegui il download
 $url = "https://raw.githubusercontent.com/nuvola-github/rundeck-nuvola/master/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
-
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
 
+# Abilita la configurazione di winrm su tutte le schede di rete
 Enable-PSremoting –SkipNetworkProfileCheck  
+
+# Abilita l'autenticazione con il protocollo CredSSP
 Enable-WSManCredSSP -Role Server -Force
 
+# Configura winrm per questa macchina
 powershell.exe -ExecutionPolicy ByPass -File $file
