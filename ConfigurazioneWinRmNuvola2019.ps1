5c379d6a13354eb22e9dea853ff1264f11639ccb
nuvola-github <53942668+nuvola-github@users.noreply.github.com>
2019-08-14 10:21:48 +0200

    Versione Nuvola

--- ConfigurazioneWinRmNuvola2019.ps1
+++ ConfigurazioneWinRmNuvola2019.ps1
@@ -1,4 +1,4 @@
-﻿$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+﻿$url = "https://github.com/nuvola-github/rundeck-nuvola/blob/master/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
