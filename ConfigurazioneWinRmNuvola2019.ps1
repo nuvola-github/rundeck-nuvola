924722791e9ee9c0db1afd8cab08faad2113e2f3
nuvola-github <53942668+nuvola-github@users.noreply.github.com>
2019-09-03 16:54:40 +0200

    Update ConfigurazioneWinRmNuvola2019.ps1

--- ConfigurazioneWinRmNuvola2019.ps1
+++ ConfigurazioneWinRmNuvola2019.ps1
@@ -1,13 +1,13 @@
-# Eseguire queste righe in Powershell ISE 4.0 
+# Eseguire queste righe in Powershell ISE 3.0 
 # AVVIATO CON PERMESSI AMMINISTRATORE
 
-# Controlla che la versionedi powershell  sia almento 4.0
+# Controlla che la versionedi powershell  sia almento 3.0
 $PSVersionTable
 
 # Abilita l'elesuzione dello script di cui verrà fatto il download
 set-executionpolicy remotesigned
 
-# Esegui il download
+# Esegui il  blocco sotto : 
 $url = "https://raw.githubusercontent.com/nuvola-github/rundeck-nuvola/master/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
